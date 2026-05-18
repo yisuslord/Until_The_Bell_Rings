9f36f79491dda35e9e26c0d23bf94299ade75c59
yisuslord <0261289@up.edu.mx>
2026-05-18 00:36:19 -0600

    Items + sonido
    Partículas de la onda expansiva + sonidos de los objetos al usarse y al recogerse

--- Assets/Script Inventario/Items/Bateria.cs
+++ Assets/Script Inventario/Items/Bateria.cs
@@ -1,24 +1,31 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
 public class Bateria : BaseItem, ICorruptible
 {
     [Header("Ajustes de Recarga")]
-    [SerializeField] private float restoreAmount = 40f; // Cuánta energía restaura al usarse
+    [SerializeField] private float restoreAmount = 40f; // CuÃ¡nta energÃ­a restaura al usarse
+    [SerializeField] private AudioClip clipRecarga;
 
     // Se ejecuta cuando el jugador presiona la Q teniendo este objeto seleccionado en la Hotbar
     public override void Use()
     {
+        /// ðŸ”¥ LLAMADA AL AUDIO MANAGER ANTES DE DESTRUIR EL OBJETO
+        if (AudioManager.Instance != null && clipRecarga != null)
+        {
+            // Usamos 2D porque es un sonido de inventario/interfaz para el jugador
+            AudioManager.Instance.PlaySFX2D(clipRecarga, 1f);
+        }
         // Buscamos el controlador de la linterna en el jugador
         FlashlightController flashlight = Object.FindFirstObjectByType<FlashlightController>();
 
         if (flashlight != null)
         {
             flashlight.RechargeBattery(restoreAmount);
-            Debug.Log($"<color=cyan>{itemName} usada con éxito.</color>");
+            Debug.Log($"<color=cyan>{itemName} usada con Ã©xito.</color>");
         }
         else
         {
-            Debug.LogWarning("No se encontró FlashlightController en la escena.");
+            Debug.LogWarning("No se encontrÃ³ FlashlightController en la escena.");
         }
     }
 }
